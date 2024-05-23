de4bfd2e9998e65e0c7167f8812e61b7b4199372
achiewe <mamukashviliachi706@gmail.com>
2024-05-23 16:45:34 +0400

    add inside the signIn page overlay

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function page() {
   return (
-    <div className="flex justify-center items-center w-full h-[600px] bg-[url('../../public/netflixBackground.png')] bg-no-repeat bg-cover"></div>
+    <div className="flex justify-center items-center w-full h-screen bg-[url('../../public/netflixBackground.png')] bg-no-repeat bg-cover relative">
+      <div className="w-full h-full bg-black absolute opacity-50"></div>
+    </div>
   );
 }
