e02ab904f51f709d2162ec07d08ddbe68b71aae8
achiewe <mamukashviliachi706@gmail.com>
2024-05-23 16:20:14 +0400

    add the background image on the signIn page

---
+++ src/app/signIn/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex justify-center items-center w-full h-[600px] bg-[url('../../public/netflixBackground.png')] bg-no-repeat bg-cover"></div>
+  );
+}
