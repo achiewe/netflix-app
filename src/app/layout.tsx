9d9b1ec6d417e976d0b3dbf04a259c518e0cb565
achiewe <mamukashviliachi706@gmail.com>
2024-05-23 13:28:16 +0400

    add inside the public file favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
       <body className={roboto.className}>{children}</body>
     </html>
   );
