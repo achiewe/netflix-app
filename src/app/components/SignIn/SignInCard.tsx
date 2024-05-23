387d113f58ec51c8d525f1305650a3a840b57541
achiewe <mamukashviliachi706@gmail.com>
2024-05-23 17:31:29 +0400

    style div and inside of it header and h1 tag

--- src/app/components/SignIn/SignInCard.tsx
+++ src/app/components/SignIn/SignInCard.tsx
@@ -1,5 +1,9 @@
 export default function SignInCard() {
   return (
-    <div className="min-h-[600px] flex flex-col px-[68px] py-[48px] bg-[#000000] opacity-70"></div>
+    <div className="min-h-[600px] flex flex-col px-[68px] py-[48px] bg-[#000000] opacity-70">
+      <header className="text-left">
+        <h1 className="font-bold text-[#ffffff] text-2xl z-50">Sign In</h1>
+      </header>
+    </div>
   );
 }
