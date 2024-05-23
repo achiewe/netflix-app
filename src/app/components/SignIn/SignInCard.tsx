81ffe64bb2ddde93630b3169ab40749526aaeebc
achiewe <mamukashviliachi706@gmail.com>
2024-05-23 16:53:01 +0400

    create component for signIn card to show it on the signIn page

---
+++ src/app/components/SignIn/SignInCard.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SignInCard() {
+  return <div>SignInCard</div>;
+}
--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function page() {
   return (
     <div className="flex justify-center items-center w-full h-screen bg-[url('../../public/netflixBackground.png')] bg-no-repeat bg-cover relative">
-      <div className="w-full h-full bg-black absolute opacity-50"></div>
+      <div className="w-full h-full bg-black absolute opacity-50" />
     </div>
   );
 }
