0344cb7213f090db37b52fc57baaf0012810851b
achiewe <mamukashviliachi706@gmail.com>
2024-05-26 17:10:05 +0400

    add rgba on the div background color to have that div opacity also

--- src/app/components/SignIn/SignInCard.tsx
+++ src/app/components/SignIn/SignInCard.tsx
@@ -2,11 +2,9 @@ import SignInInput from "./SignInInput";
 
 export default function SignInCard() {
   return (
-    <div className="min-h-[600px] flex flex-col px-[68px] py-[48px] bg-[#000000] opacity-70">
+    <div className="min-h-[600px] flex flex-col px-[68px] py-[48px] bg-[rgba(0,0,0,0.7)]">
       <header className="text-left">
-        <h1 className="font-bold text-[#ffffff] text-2xl z-50 mb-[28px]">
-          Sign In
-        </h1>
+        <h1 className="font-bold text-[#ffffff] text-2xl mb-[28px]">Sign In</h1>
       </header>
       <SignInInput />
     </div>
