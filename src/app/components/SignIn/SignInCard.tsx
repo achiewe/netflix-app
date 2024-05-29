d61800143a60f876134c5468c14a17f92c727638
achiewe <mamukashviliachi706@gmail.com>
2024-05-29 11:13:19 +0400

    add the z index on the SignInCard container

--- src/app/components/SignIn/SignInCard.tsx
+++ src/app/components/SignIn/SignInCard.tsx
@@ -3,7 +3,7 @@ import SignInInput from "./SignInInput";
 // sign in Card
 export default function SignInCard() {
   return (
-    <div className="min-h-[600px] flex flex-col px-[68px] py-[48px] bg-[rgba(0,0,0,0.7)]">
+    <div className="min-h-[600px] flex flex-col px-[68px] py-[48px] bg-[rgba(0,0,0,0.7)] z-10">
       <header className="text-left">
         <h1 className="font-bold text-[#ffffff] text-2xl mb-[28px]">Sign In</h1>
       </header>
