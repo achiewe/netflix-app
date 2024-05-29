836525b65fc07e225accf20de41e59ff98a1103b
achiewe <mamukashviliachi706@gmail.com>
2024-05-29 11:10:27 +0400

    add the outline none on the input

--- src/app/components/costum/CustomInput.tsx
+++ src/app/components/costum/CustomInput.tsx
@@ -11,7 +11,7 @@ function CustomInput({ inputType, inputPlaceholder }: inputProps): JSX.Element {
     <input
       type={inputType}
       placeholder={inputPlaceholder}
-      className="w-full text-inherit px-[10px] py-[20px] rounded-[10px] border-stone-600 bg-red-900 z-10"
+      className="outline-none w-full text-inherit px-[10px] py-[20px] rounded-[10px] border-stone-600 bg-red-900 z-10"
     />
   );
 }
