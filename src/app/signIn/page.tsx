edee6721094842a7439e5f15e4b5ab33614fdd62
achiewe <mamukashviliachi706@gmail.com>
2024-05-31 12:31:56 +0400

    console.log usestate to see that meaning

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -4,8 +4,10 @@ import React, { useState } from "react";
 import SignInCard from "../components/SignIn/SignInCard";
 
 // SignIn function
-console.log(useState, "sadasdasdasd");
+
 export default function page() {
+  const [state, setState] = useState(2);
+  console.log(useState, "asdasd");
   return (
     <div className="flex justify-center items-center w-full h-screen bg-[url('../../public/netflixBackground.png')] bg-no-repeat bg-cover relative">
       <div className="w-full h-full bg-black absolute opacity-50" />
