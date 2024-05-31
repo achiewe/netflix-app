cfafb027ff9d34989193b4d5d46a50790d14f748
achiewe <mamukashviliachi706@gmail.com>
2024-05-31 11:46:51 +0400

    ad useState inside the page tsx file

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import SignInCard from "../components/SignIn/SignInCard";
 
 // SignIn function
+console.log(useState, "sadasdasdasd");
 export default function page() {
   return (
     <div className="flex justify-center items-center w-full h-screen bg-[url('../../public/netflixBackground.png')] bg-no-repeat bg-cover relative">
