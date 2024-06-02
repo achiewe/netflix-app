7d2e786a428295021f870d89b177b1d04dcb287d
achiewe <mamukashviliachi706@gmail.com>
2024-06-02 13:49:39 +0400

    export myContext to use it in another comppoent

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 
-import React, { useCallback, useMemo, useRef, useState } from "react";
+import React, {
+  createContext,
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import SignInCard from "../components/SignIn/SignInCard";
-import { createContext } from "vm";
 
 // SignIn function
 
 export default function page() {
   let name = "achi";
-  const myContext = createContext();
+  export default const myContext = createContext("genodi");
   return (
     <myContext.Provider value={name}>
       <div className="flex justify-center items-center w-full h-screen bg-[url('../../public/netflixBackground.png')] bg-no-repeat bg-cover relative">
