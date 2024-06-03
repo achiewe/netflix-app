2911d43eafe75c3df123321cf3cde0471e5b1a7a
achiewe <mamukashviliachi706@gmail.com>
2024-06-03 14:30:39 +0400

    remove hooks from the component

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,28 +1,18 @@
 "use client";
 
-import React, {
-  createContext,
-  useCallback,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
+import React from "react";
 import SignInCard from "../components/SignIn/SignInCard";
 
 // SignIn function
 
 export default function page() {
-  let name = "achi";
-  export default const myContext = createContext("genodi");
   return (
-    <myContext.Provider value={name}>
-      <div className="flex justify-center items-center w-full h-screen bg-[url('../../public/netflixBackground.png')] bg-no-repeat bg-cover relative">
-        <div className="w-full h-full bg-black absolute opacity-50" />
-        <SignInCard />
-        <button style={{ backgroundColor: "red", width: 300, height: 300 }}>
-          achi
-        </button>
-      </div>
-    </myContext.Provider>
+    <div className="flex justify-center items-center w-full h-screen bg-[url('../../public/netflixBackground.png')] bg-no-repeat bg-cover relative">
+      <div className="w-full h-full bg-black absolute opacity-50" />
+      <SignInCard />
+      <button style={{ backgroundColor: "red", width: 300, height: 300 }}>
+        achi
+      </button>
+    </div>
   );
 }
