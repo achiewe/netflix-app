87afc779c83b4260ffc3ce4a9c439999f8ef79d2
achiewe <mamukashviliachi706@gmail.com>
2024-06-06 11:57:35 +0400

    add the p tag inside the SignInButton

--- src/app/components/SignIn/SignInButton.tsx
+++ src/app/components/SignIn/SignInButton.tsx
@@ -5,6 +5,7 @@ export default function SignInButton() {
   return (
     <div className='w-full flex flex-col gap-[16px]'>
       <CustomButton buttonName="Sign In" bgColor='red'/>
+      <p className='text-[white] font-normal opacity-70 text-center'>OR</p>
     </div>
   )
 }
