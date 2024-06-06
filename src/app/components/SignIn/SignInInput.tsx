f8285056ee3f5b61fe677834ad5e82e65496ae4f
achiewe <mamukashviliachi706@gmail.com>
2024-06-06 17:56:25 +0400

    change the type of the input

--- src/app/components/SignIn/SignInInput.tsx
+++ src/app/components/SignIn/SignInInput.tsx
@@ -5,8 +5,8 @@ import CustomInput from "../costum/CustomInput";
 export default function SignInInput() {
   return (
     <div className="flex flex-col gap-[20px]">
-      <CustomInput inputType="text" inputPlaceholder="Email or mobile number" />
-      <CustomInput inputType="text" inputPlaceholder="Email or mobile number" />
+      <CustomInput inputType="email" inputPlaceholder="Email or mobile number" />
+      <CustomInput inputType="password" inputPlaceholder="password" />
     </div>
   );
 }
