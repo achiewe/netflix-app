cbf70ec85400461fb7e9a23db9f93c32feb15e6b
achiewe <mamukashviliachi706@gmail.com>
2024-06-06 11:46:01 +0400

    add the buttonName props type

--- src/app/components/costum/CustomButton.tsx
+++ src/app/components/costum/CustomButton.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-export default function CustomButton({buttonName}) {
+interface CustomButtonProps{
+buttonName:string
+}
+
+export default function CustomButton({buttonName}:CustomButtonProps) {
   return (
     <button className='bg-[red] w-full text-white h-[40px] rounded-[5px] font-bold cursor-pointer'> {buttonName} </button>
   )
