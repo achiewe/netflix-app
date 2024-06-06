8887c54fbee61aaaaecc74458e0f158f0233d519
achiewe <mamukashviliachi706@gmail.com>
2024-06-06 17:48:20 +0400

    add the hover on the buttons

--- src/app/components/costum/CustomButton.tsx
+++ src/app/components/costum/CustomButton.tsx
@@ -7,6 +7,6 @@ bgColor:string
 
 export default function CustomButton({buttonName, bgColor}:CustomButtonProps) {
   return (
-    <button className={`bg-[${bgColor}] w-full text-white h-[40px] rounded-[5px] font-bold cursor-pointer`}> {buttonName} </button>
+    <button className={`bg-[${bgColor}] w-full text-white h-[40px] rounded-[5px] font-bold cursor-pointer hover:bg-red-900`}> {buttonName} </button>
   )
 }
