dd387bf9ded8d58b7ec71e8f1401e45fb14f51f8
achiewe <mamukashviliachi706@gmail.com>
2024-06-06 11:44:47 +0400

    add the props inside the customButton content and bring it in the parent component

--- src/app/components/SignIn/SignInButton.tsx
+++ src/app/components/SignIn/SignInButton.tsx
@@ -4,7 +4,7 @@ import CustomButton from '../costum/CustomButton'
 export default function SignInButton() {
   return (
     <div className='w-full flex flex-col gap-[16px]'>
-      <CustomButton/>
+      <CustomButton buttonName="Sign In"/>
     </div>
   )
 }
--- src/app/components/costum/CustomButton.tsx
+++ src/app/components/costum/CustomButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function CustomButton() {
+export default function CustomButton({buttonName}) {
   return (
-    <button className='bg-[red] w-full text-white h-[40px] rounded-[5px] font-bold cursor-pointer'> Sign In </button>
+    <button className='bg-[red] w-full text-white h-[40px] rounded-[5px] font-bold cursor-pointer'> {buttonName} </button>
   )
 }
