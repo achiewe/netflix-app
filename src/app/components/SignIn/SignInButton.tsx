16764e14b5adabe9ce38f5179e06ce95c3860222
achiewe <mamukashviliachi706@gmail.com>
2024-06-07 17:50:01 +0400

    add the bgColor on the CustomButtons

--- src/app/components/SignIn/SignInButton.tsx
+++ src/app/components/SignIn/SignInButton.tsx
@@ -3,11 +3,11 @@
 import React from 'react'
 import CustomButton from '../costum/CustomButton'
 
-// signInButton component
+
 export default function SignInButton() {
   return (
     <div className='w-full flex flex-col gap-[16px]'>
-      <CustomButton buttonName="Sign In" bgColor='red'/>
+      <CustomButton buttonName="Sign In" bgColor='#722F37'/>
       <p className='text-[white] font-normal opacity-70 text-center'>OR</p>
       <CustomButton buttonName="Sign Up" bgColor='gray'/>
     </div>
