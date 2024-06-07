1746006eeeddee98655df72096bfe305887a0314
achiewe <mamukashviliachi706@gmail.com>
2024-06-07 18:41:53 +0400

    change button background color

--- src/app/components/SignIn/SignInButton.tsx
+++ src/app/components/SignIn/SignInButton.tsx
@@ -7,9 +7,9 @@ import CustomButton from '../costum/CustomButton'
 export default function SignInButton() {
   return (
     <div className='w-full flex flex-col gap-[16px]'>
-      <CustomButton buttonName="Sign In"/>
+      <CustomButton buttonName="Sign In" btnColor="red"/>
       <p className='text-[white] font-normal opacity-70 text-center'>OR</p>
-      <CustomButton buttonName="Sign Up"/>
+      <CustomButton buttonName="Sign Up" btnColor="green"/>
     </div>
   )
 }
--- src/app/components/costum/CustomButton.tsx
+++ src/app/components/costum/CustomButton.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 interface CustomButtonProps{
 buttonName:string
+btnColor:string
 }
 
-export default function CustomButton({buttonName}:CustomButtonProps) {
+export default function CustomButton({buttonName, btnColor}:CustomButtonProps) {
   return (
-    <button className={`w-full text-white h-[40px] rounded-[5px] font-bold cursor-pointer hover:opacity-60`}> {buttonName} </button>
+    <button className={`w-full text-white bg-[${btnColor}] h-[40px] rounded-[5px] font-bold cursor-pointer hover:opacity-60`}> {buttonName} </button>
   )
 }
