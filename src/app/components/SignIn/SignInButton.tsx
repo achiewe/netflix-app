60f38293ee2a51fbd038a3069b5ad8c59b0a29f1
achiewe <mamukashviliachi706@gmail.com>
2024-06-07 18:06:05 +0400

    remove backgrund color

--- src/app/components/SignIn/SignInButton.tsx
+++ src/app/components/SignIn/SignInButton.tsx
@@ -7,9 +7,9 @@ import CustomButton from '../costum/CustomButton'
 export default function SignInButton() {
   return (
     <div className='w-full flex flex-col gap-[16px]'>
-      <CustomButton buttonName="Sign In" bgColor='#722F37'/>
+      <CustomButton buttonName="Sign In"/>
       <p className='text-[white] font-normal opacity-70 text-center'>OR</p>
-      <CustomButton buttonName="Sign Up" bgColor='gray'/>
+      <CustomButton buttonName="Sign Up"/>
     </div>
   )
 }
