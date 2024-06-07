56bf4a748c840e063ab98836c9a2b2b45fba45de
achiewe <mamukashviliachi706@gmail.com>
2024-06-07 18:44:24 +0400

    add on the button background color red and remove the props from there

--- src/app/components/costum/CustomButton.tsx
+++ src/app/components/costum/CustomButton.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 interface CustomButtonProps{
 buttonName:string
-btnColor:string
 }
 
-export default function CustomButton({buttonName, btnColor}:CustomButtonProps) {
+export default function CustomButton({buttonName}:CustomButtonProps) {
   return (
-    <button className={`w-full text-white bg-[${btnColor}] h-[40px] rounded-[5px] font-bold cursor-pointer hover:opacity-60`}> {buttonName} </button>
+    <button className={`w-full text-white bg-[red] h-[40px] rounded-[5px] font-bold cursor-pointer hover:opacity-60`}> {buttonName} </button>
   )
 }
