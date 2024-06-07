e86009c72bbecef685bc2b3495593ceecc733b82
achiewe <mamukashviliachi706@gmail.com>
2024-06-07 18:45:10 +0400

    remove button background color from the custom button

--- src/app/components/SignIn/SignInButton.tsx
+++ src/app/components/SignIn/SignInButton.tsx
@@ -7,9 +7,9 @@ import CustomButton from '../costum/CustomButton'
 export default function SignInButton() {
   return (
     <div className='w-full flex flex-col gap-[16px]'>
-      <CustomButton buttonName="Sign In" btnColor="red"/>
+      <CustomButton buttonName="Sign In"/>
       <p className='text-[white] font-normal opacity-70 text-center'>OR</p>
-      <CustomButton buttonName="Sign Up" btnColor="green"/>
+      <CustomButton buttonName="Sign Up"/>
     </div>
   )
 }
