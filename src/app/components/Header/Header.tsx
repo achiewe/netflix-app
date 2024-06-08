42728e33cb02e92870da6d89b4d923d74e6edfb0
achiewe <mamukashviliachi706@gmail.com>
2024-06-08 11:41:36 +0400

    add the netflix logo in the header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import netflixLogo from "../../../../public/netflixLogo.png";
+import Image from 'next/image';
 export default function Header() {
   return (
-    <div>
-      
-    </div>
+    <header className='flex flex-row w-full items-center justify-between px-[15px]'>
+   <Image src={netflixLogo} alt='netflix logo' className='w-[150px] h-[100px]'/>
+    </header>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import Header from "./components/Header/Header";
+
 export default function Home() {
-  return <div className="w-full min-h-screen flex flex-col bg-[#000000]"></div>;
+  return <div className="w-full min-h-screen flex flex-col bg-[#000000]">
+    <Header/>
+  </div>;
 }
