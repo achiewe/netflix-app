4067353e1afeee9eb34313bfbb417ee48db8a7d8
achiewe <mamukashviliachi706@gmail.com>
2024-06-08 11:56:00 +0400

    add the cursor pointer on the netflix logo

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Header() {
   return (
     <header className='flex flex-row w-full items-center justify-between px-[15px]'>
-   <Image src={netflixLogo} alt='netflix logo' className='w-[150px] h-[100px]'/>
+   <Image src={netflixLogo} alt='netflix logo' className='w-[150px] h-[100px] cursor-pointer'/>
     </header>
   )
 }
