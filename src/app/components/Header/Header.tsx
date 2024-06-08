49c0529b05b7c122e199a0500cf39d4e728ee666
achiewe <mamukashviliachi706@gmail.com>
2024-06-08 12:02:23 +0400

    add the burger menu logo inside the header component

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import netflixLogo from "../../../../public/assets/netflixLogo.png";
+import burgerMenu from "../../../../public/assets/burgerMenu.png";
 import Image from 'next/image';
 export default function Header() {
   return (
     <header className='flex flex-row w-full items-center justify-between px-[15px]'>
-   <Image src={netflixLogo} alt='netflix logo' className='w-[150px] h-[100px] cursor-pointer'/>
+   <Image src={netflixLogo} alt='netflix logo' className='w-[150px] h-[80px] cursor-pointer'/>
+   <Image src={burgerMenu} alt='burger logo' className='w-[40px] h-[40px] cursor-pointer'/>
     </header>
   )
 }
