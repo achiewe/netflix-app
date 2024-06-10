9b1d46d9c40267695cc6cdabdeb17c2ba1d269e7
achiewe <mamukashviliachi706@gmail.com>
2024-06-10 21:30:40 +0400

    pass the onclick function from child component to use it for onclick on the button

--- src/app/components/costum/CustomButton.tsx
+++ src/app/components/costum/CustomButton.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 interface CustomButtonProps{
-buttonName:string
+buttonName:string;
+onClick: () => void;
 }
 
-export default function CustomButton({buttonName}:CustomButtonProps) {
+export default function CustomButton({buttonName, onClick}:CustomButtonProps) {
   return (
-    <button className={`w-full text-white bg-[red] h-[40px] rounded-[5px] font-bold cursor-pointer hover:opacity-60`}> {buttonName} </button>
+    <button className={`w-full text-white bg-[red] h-[40px] rounded-[5px] font-bold cursor-pointer hover:opacity-60`} onClick={onClick}> {buttonName} </button>
   )
 }
