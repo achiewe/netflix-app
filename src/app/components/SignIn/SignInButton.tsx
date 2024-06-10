30708c46f84e3781549b58e41203802f7ea944a7
achiewe <mamukashviliachi706@gmail.com>
2024-06-10 21:58:25 +0400

    add the handle clicks on the custom buttons

--- src/app/components/SignIn/SignInButton.tsx
+++ src/app/components/SignIn/SignInButton.tsx
@@ -6,11 +6,24 @@ import CustomButton from '../costum/CustomButton'
 
 // SignInButton component
 export default function SignInButton() {
+  const pathname = window.location.pathname;
+  console.log(pathname);
+
+  const handleSignInClick = () => {
+    console.log("Sign In button clicked");
+    
+    // Add your sign-in logic here
+  };
+
+  const handleSignUpClick = () => {
+    console.log("Sign Up button clicked");
+    // Add your sign-up logic here
+  };
   return (
     <div className='w-full flex flex-col gap-[16px]'>
-      <CustomButton buttonName="Sign In"/>
+      <CustomButton buttonName="Sign In" onClick={handleSignInClick}/>
       <p className='text-[white] font-normal opacity-70 text-center'>OR</p>
-      <CustomButton buttonName="Sign Up"/>
+      <CustomButton buttonName="Sign Up" onClick={handleSignUpClick}/>
     </div>
   )
 }
