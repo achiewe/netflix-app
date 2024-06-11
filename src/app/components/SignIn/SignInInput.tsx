59f3b96ba1eeb8e8e2528979efeab98c291ef0c9
achiewe <mamukashviliachi706@gmail.com>
2024-06-11 12:40:06 +0400

    create useState to take the value of the input

--- src/app/components/SignIn/SignInInput.tsx
+++ src/app/components/SignIn/SignInInput.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import CustomInput from "../costum/CustomInput";
 
 // SignInInput function
 export default function SignInInput() {
+
+  const [inputValue, setINputValue] = useState<string>("");
+
   return (
     <div className="flex flex-col gap-[20px]">
       <CustomInput inputType="email" inputPlaceholder="Email or mobile number" onChange={(e) => {
