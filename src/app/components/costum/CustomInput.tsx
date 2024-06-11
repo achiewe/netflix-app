4a5f4fc1e13ca4a41802ed7c2564b669e3ad722d
achiewe <mamukashviliachi706@gmail.com>
2024-06-11 11:43:10 +0400

    pass the onchange event on the input to see the value of the input

--- src/app/components/costum/CustomInput.tsx
+++ src/app/components/costum/CustomInput.tsx
@@ -3,12 +3,14 @@ import React from "react";
 interface inputProps {
   inputType: string;
   inputPlaceholder: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 // custom function inside the customInput component
-function CustomInput({ inputType, inputPlaceholder }: inputProps): JSX.Element {
+function CustomInput({ inputType, inputPlaceholder, onChange }: inputProps): JSX.Element {
   return (
     <input
+    onChange={onChange}
       type={inputType}
       placeholder={inputPlaceholder}
       className="z-50 border outline-none w-full placeholder:text-slate-400 text-[#ffffff] px-[10px] py-[20px] rounded-[5px] bg-[rgba(54,54,54,0.3)]"
