eeb183122da2941f0792156f4f195126044ff4d5
achiewe <mamukashviliachi706@gmail.com>
2024-06-11 12:41:12 +0400

    pass the value property on customInput to use it in the component

--- src/app/components/costum/CustomInput.tsx
+++ src/app/components/costum/CustomInput.tsx
@@ -3,15 +3,17 @@ import React from "react";
 interface inputProps {
   inputType: string;
   inputPlaceholder: string;
+  value:string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 // custom function inside the customInput component
-function CustomInput({ inputType, inputPlaceholder, onChange }: inputProps): JSX.Element {
+function CustomInput({ inputType, inputPlaceholder, onChange, value }: inputProps): JSX.Element {
   return (
     <input
     onChange={onChange}
       type={inputType}
+      value={value}
       placeholder={inputPlaceholder}
       className="z-50 border outline-none w-full placeholder:text-slate-400 text-[#ffffff] px-[10px] py-[20px] rounded-[5px] bg-[rgba(54,54,54,0.3)]"
     />
