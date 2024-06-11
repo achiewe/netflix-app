bd8ad81305c1fbed0cfb84335696e5bfc73fec02
achiewe <mamukashviliachi706@gmail.com>
2024-06-11 11:45:51 +0400

    add on the input onchange

--- src/app/components/SignIn/SignInInput.tsx
+++ src/app/components/SignIn/SignInInput.tsx
@@ -5,8 +5,12 @@ import CustomInput from "../costum/CustomInput";
 export default function SignInInput() {
   return (
     <div className="flex flex-col gap-[20px]">
-      <CustomInput inputType="email" inputPlaceholder="Email or mobile number" />
-      <CustomInput inputType="password" inputPlaceholder="password" />
+      <CustomInput inputType="email" inputPlaceholder="Email or mobile number" onChange={(e) => {
+        e.target.value
+      }}/>
+      <CustomInput inputType="password" inputPlaceholder="password" onChange={(e) => {
+        e.target.value
+      }}/>
     </div>
   );
 }
