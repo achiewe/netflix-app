3f782ba4c11a90e13a54a57e4577a166bca2fd71
achiewe <mamukashviliachi706@gmail.com>
2024-06-12 17:29:24 +0400

    add the hover on the Link element style

--- src/app/components/BurgerFrame/BurgerFrame.tsx
+++ src/app/components/BurgerFrame/BurgerFrame.tsx
@@ -8,18 +8,18 @@ export default function BurgerFrame() {
     const openFrame = useSelector((store:RootState) => store.openFrame.openFrame)
   return (
     <div className={`w-[100%] h-[254px] bg-[red] ${openFrame ? "fixed" : "absolute"} top-[80px] overflow-x-hidden ${openFrame ? "right-[-100%]" : "right-0"} overflow-hidden flex flex-col justify-center items-center gap-[20px] duration-500`}>
-     <Link href="/TVShows" className='text-[20px] font-extrabold'>
+     <Link href="/TVShows" className='text-[20px] font-extrabold hover:text-[white]'>
      TV Shows
      </Link>
-      
-     <Link href="/Movies" className='text-[20px] font-extrabold'>
+
+     <Link href="/Movies" className='text-[20px] font-extrabold hover:text-[white]'>
      Movies
      </Link>
   
-    <Link href="/New&Popular" className='text-[20px] font-extrabold'>
+    <Link href="/New&Popular" className='text-[20px] font-extrabold hover:text-[white]'>
     New & Popular
     </Link>
-    <Link href="/MyList" className='text-[20px] font-extrabold'>
+    <Link href="/MyList" className='text-[20px] font-extrabold hover:text-[white]'>
     My List
     </Link>
     </div>
