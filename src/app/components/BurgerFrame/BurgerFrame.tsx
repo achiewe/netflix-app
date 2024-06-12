4063f2a57cdb6ceec1b4a39f5df9b0eaa36bf325
achiewe <mamukashviliachi706@gmail.com>
2024-06-12 17:23:46 +0400

    add the pages which should have website and change the route

--- src/app/components/BurgerFrame/BurgerFrame.tsx
+++ src/app/components/BurgerFrame/BurgerFrame.tsx
@@ -1,13 +1,25 @@
 "use client";
 import { RootState } from '@/app/redux/features/store';
+import Link from 'next/link';
 import React from 'react'
 import { useSelector } from 'react-redux';
 
 export default function BurgerFrame() {
     const openFrame = useSelector((store:RootState) => store.openFrame.openFrame)
   return (
-    <div className={`w-[100%] h-[254px] bg-[red] ${openFrame ? "fixed" : "absolute"} top-[80px] overflow-x-hidden ${openFrame ? "right-[-100%]" : "right-0"} overflow-hidden flex flex-col justify-center items-center gap-[20px] duration-500`}>
-      
+    <div className={`w-[100%] h-[254px] bg-[red] ${openFrame ? "fixed" : "absolute"} top-[80px] overflow-x-hidden ${openFrame ? "right-[-100%]" : "right-0"} overflow-hidden flex flex-col justify-center items-center gap-[10px] duration-500`}>
+     <Link href="/TVShows">
+     TV Shows
+     </Link>
+        Movies
+     <Link href="/Movies">
+     </Link>
+    New & Popular
+    <Link href="/New&Popular">
+    </Link>
+    <Link href="/MyList">
+    My List
+    </Link>
     </div>
   )
 }
