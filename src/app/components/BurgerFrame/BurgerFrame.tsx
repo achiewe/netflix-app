1fe20a61f29770d27df797e99e6c16141317d7eb
achiewe <mamukashviliachi706@gmail.com>
2024-06-12 16:45:18 +0400

    add the burgerFrame component inside the header component

--- src/app/components/BurgerFrame/BurgerFrame.tsx
+++ src/app/components/BurgerFrame/BurgerFrame.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function BurgerFrame() {
   return (
-    <div className='w-[100%] h-[254px] bg-[red] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+    <div className='w-[100%] h-[254px] bg-[red]'>
       
     </div>
   )
--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,17 +1,17 @@
 "use client";
-
 import React from 'react'
 import netflixLogo from "../../../../public/assets/netflixLogo.png";
 import Image from 'next/image';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/app/redux/features/store';
 import { setOpenFrame } from '@/app/redux/features/OpenBurgerSlice';
+import BurgerFrame from '../BurgerFrame/BurgerFrame';
 export default function Header() {
 const openFrame = useSelector((store:RootState) => store.openFrame.openFrame)
  const dispatch = useDispatch()
  console.log(openFrame)
   return (
-    <header className='flex flex-row w-full items-center justify-between px-[15px]'>
+    <header className='flex flex-row w-full items-center justify-between px-[15px] relative'>
    <Image src={netflixLogo} alt='netflix logo' className='w-[150px] h-[80px] cursor-pointer' onClick={() => {
     location.reload()
    }}/>
@@ -22,6 +22,7 @@ const openFrame = useSelector((store:RootState) => store.openFrame.openFrame)
 <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"/>
 <g id="SVGRepo_iconCarrier"> <path fillRule="evenodd" clipRule="evenodd" d="M4 5C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H4ZM7 12C7 11.4477 7.44772 11 8 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H8C7.44772 13 7 12.5523 7 12ZM13 18C13 17.4477 13.4477 17 14 17H20C20.5523 17 21 17.4477 21 18C21 18.5523 20.5523 19 20 19H14C13.4477 19 13 18.5523 13 18Z" fill="#ffffff"/> </g>
 </svg>
+<BurgerFrame/>
     </header>
   )
 }
