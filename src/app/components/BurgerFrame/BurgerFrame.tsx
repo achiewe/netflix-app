336543eafecdc5de2e3fe4283ce144582a2d9c38
achiewe <mamukashviliachi706@gmail.com>
2024-06-12 17:05:18 +0400

    correct the duration time on the burger menu

--- src/app/components/BurgerFrame/BurgerFrame.tsx
+++ src/app/components/BurgerFrame/BurgerFrame.tsx
@@ -6,7 +6,7 @@ import { useSelector } from 'react-redux';
 export default function BurgerFrame() {
     const openFrame = useSelector((store:RootState) => store.openFrame.openFrame)
   return (
-    <div className={`w-[100%] h-[254px] bg-[red] ${openFrame ? "fixed" : "absolute"} top-[80px] overflow-x-hidden ${openFrame ? "right-[-100%]" : "right-0"} overflow-hidden flex flex-col justify-center items-center gap-[20px] transition duration-300`}>
+    <div className={`w-[100%] h-[254px] bg-[red] ${openFrame ? "fixed" : "absolute"} top-[80px] overflow-x-hidden ${openFrame ? "right-[-100%]" : "right-0"} overflow-hidden flex flex-col justify-center items-center gap-[20px] duration-500`}>
       
     </div>
   )
