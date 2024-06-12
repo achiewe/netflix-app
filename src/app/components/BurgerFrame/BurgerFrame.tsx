200254ebfcda2b3bda9f0a614f8c859a3d50a239
achiewe <mamukashviliachi706@gmail.com>
2024-06-12 16:52:58 +0400

    style burgerFrame

--- src/app/components/BurgerFrame/BurgerFrame.tsx
+++ src/app/components/BurgerFrame/BurgerFrame.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function BurgerFrame() {
   return (
-    <div className='w-[100%] h-[254px] bg-[red]'>
+    <div className='w-[100%] h-[254px] bg-[red] absolute top-[72px] overflow-hidden flex flex-col justify-center items-center gap-[20px] transition duration-300'>
       
     </div>
   )
