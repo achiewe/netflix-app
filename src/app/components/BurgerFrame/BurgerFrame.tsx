09e3b633da3028f380426dc6a18da4ff0c4cc306
achiewe <mamukashviliachi706@gmail.com>
2024-06-12 16:18:14 +0400

    add the BurgerFrame in the page tsx file

--- src/app/components/BurgerFrame/BurgerFrame.tsx
+++ src/app/components/BurgerFrame/BurgerFrame.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function BurgerFrame() {
   return (
-    <div>
+    <div className='w-[200px] h-[400px] bg-[red]'>
       
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import BurgerFrame from "./components/BurgerFrame/BurgerFrame";
 import Header from "./components/Header/Header";
 
 export default function Home() {
   return <div className="w-full min-h-screen flex flex-col bg-[#000000]">
     <Header/>
+    <BurgerFrame/>
   </div>;
 }
