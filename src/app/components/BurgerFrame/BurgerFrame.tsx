434e316ff22d962a8ca4f55cd9efd3926e1ec7bb
achiewe <mamukashviliachi706@gmail.com>
2024-06-12 17:03:16 +0400

    add the logic that if the openFrame boolean logic is true then show the burger menu and if its false hide it

--- src/app/components/BurgerFrame/BurgerFrame.tsx
+++ src/app/components/BurgerFrame/BurgerFrame.tsx
@@ -1,8 +1,12 @@
+"use client";
+import { RootState } from '@/app/redux/features/store';
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 export default function BurgerFrame() {
+    const openFrame = useSelector((store:RootState) => store.openFrame.openFrame)
   return (
-    <div className='w-[100%] h-[254px] bg-[red] absolute top-[72px] overflow-hidden flex flex-col justify-center items-center gap-[20px] transition duration-300'>
+    <div className={`w-[100%] h-[254px] bg-[red] ${openFrame ? "fixed" : "absolute"} top-[80px] overflow-x-hidden ${openFrame ? "right-[-100%]" : "right-0"} overflow-hidden flex flex-col justify-center items-center gap-[20px] transition duration-300`}>
       
     </div>
   )
