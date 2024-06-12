c541d3fe5d4d724c0b34929c175def44e0d7413f
achiewe <mamukashviliachi706@gmail.com>
2024-06-12 16:19:45 +0400

    import BurgerFrame inside the main page tsx file

--- src/app/components/BurgerFrame/BurgerFrame.tsx
+++ src/app/components/BurgerFrame/BurgerFrame.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function BurgerFrame() {
   return (
-    <div className='w-[200px] h-[400px] bg-[red]'>
+    <div className='w-[200px] h-[400px] bg-[red] absolute left-0 right-0 top-0 bottom-0'>
       
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import BurgerFrame from "./components/BurgerFrame/BurgerFrame";
 import Header from "./components/Header/Header";
 
 export default function Home() {
-  return <div className="w-full min-h-screen flex flex-col bg-[#000000]">
+  return <div className="w-full min-h-screen flex flex-col bg-[#000000] relative">
     <Header/>
     <BurgerFrame/>
   </div>;
