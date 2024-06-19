2badac511bb24512fed66836223a230a5ba5921d
achiewe <mamukashviliachi706@gmail.com>
2024-06-19 10:50:53 +0400

    import signUpCard component inside the page tsx file

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import SignUpCard from '../components/SignUp/SignUpCard'
 
 export default function page() {
   return (
     <div className="flex justify-center items-center w-full h-screen bg-[url('../../public/assets/netflixBackground.png')] bg-no-repeat bg-cover relative">
     <div className="w-full h-full bg-black absolute opacity-50" />
-  
+    <SignUpCard/>
   </div>
   )
 }
