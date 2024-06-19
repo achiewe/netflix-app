b7311eaafbdde719aa15b66b6010fb11e36fb7f4
achiewe <mamukashviliachi706@gmail.com>
2024-06-19 10:50:14 +0400

    style signUp page tsx

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      
-    </div>
+    <div className="flex justify-center items-center w-full h-screen bg-[url('../../public/assets/netflixBackground.png')] bg-no-repeat bg-cover relative">
+    <div className="w-full h-full bg-black absolute opacity-50" />
+  
+  </div>
   )
 }
