d8b61c638b1ad90e70a3aa69e8a90778d693badf
achiewe <mamukashviliachi706@gmail.com>
2024-06-19 11:04:27 +0400

    change signInCard element div with form element

--- src/app/components/SignIn/SignInCard.tsx
+++ src/app/components/SignIn/SignInCard.tsx
@@ -4,7 +4,7 @@ import SignInInput from "./SignInInput";
 // sign in Card
 export default function SignInCard() {
   return (
-    <div className="w-full min-h-[600px] flex max-w-[450px] flex-col px-[68px] py-[48px] bg-[rgba(0,0,0,0.7)] z-10">
+    <form className="w-full min-h-[600px] flex max-w-[450px] flex-col px-[68px] py-[48px] bg-[rgba(0,0,0,0.7)] z-10">
       <header className="text-left">
         <h1 className="font-bold text-[#ffffff] text-2xl mb-[28px]">Sign In</h1>
       </header>
@@ -12,6 +12,6 @@ export default function SignInCard() {
       <SignInInput />
       <SignInButton/>
       </div>
-    </div>
+    </form>
   );
 }
--- src/app/components/SignUp/SignUpInputs.tsx
+++ src/app/components/SignUp/SignUpInputs.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import CustomInput from '../costum/CustomInput'
 
 export default function SignUpInputs() {
   return (
-    <div>
-      
+    <div className="flex flex-col gap-[20px]">
+      <CustomInput inputType="email" value={emailValue} inputPlaceholder="Email or mobile number" onChange={(e) => {
+        setEmailValue(e.target.value)
+      }}/>
+      <CustomInput inputType="password" value={passwordValue} inputPlaceholder="password" onChange={(e) => {
+        setPasswordValue(e.target.value)
+      }}/>
+      <CustomInput inputType="password" value={passwordValue} inputPlaceholder="password" onChange={(e) => {
+        setPasswordValue(e.target.value)
+      }}/>
     </div>
   )
 }
