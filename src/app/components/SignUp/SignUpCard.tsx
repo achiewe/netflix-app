0809a3f54a2a55612cffc6a6c9ee426a645a496d
achiewe <mamukashviliachi706@gmail.com>
2024-06-19 10:46:24 +0400

    style the signUpCard

--- src/app/components/SignUp/SignUpCard.tsx
+++ src/app/components/SignUp/SignUpCard.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default function SignUpCard() {
   return (
     <div>
-      
+       <div className="w-full min-h-[600px] flex max-w-[450px] flex-col px-[68px] py-[48px] bg-[rgba(0,0,0,0.7)] z-10">
+      <header className="text-left">
+        <h1 className="font-bold text-[#ffffff] text-2xl mb-[28px]">Sign In</h1>
+      </header>
+      <div className="w-full flex flex-col gap-[16px]">
+   
+      </div>
+    </div>
     </div>
   )
 }
