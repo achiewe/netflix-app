fd73ee1aec971192f0be4909ac9a4c283b5cdc3e
achiewe <mamukashviliachi706@gmail.com>
2024-06-19 11:14:12 +0400

    change and add the form element inside the signUpCard component

--- src/app/components/SignUp/SignUpCard.tsx
+++ src/app/components/SignUp/SignUpCard.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function SignUpCard() {
   return (
-       <div className="w-full min-h-[600px] flex max-w-[450px] flex-col px-[68px] py-[48px] bg-[rgba(0,0,0,0.7)] z-10">
+    <form className="w-full min-h-[600px] flex max-w-[450px] flex-col px-[68px] py-[48px] bg-[rgba(0,0,0,0.7)] z-10">
       <header className="text-left">
         <h1 className="font-bold text-[#ffffff] text-2xl mb-[28px]">Sign Up</h1>
       </header>
       <div className="w-full flex flex-col gap-[16px]">
    
       </div>
-    </div>
+    </form>
   )
 }
