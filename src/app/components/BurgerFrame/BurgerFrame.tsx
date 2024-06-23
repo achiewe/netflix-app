2534d6deb96a3d1334a4600deed2c6d61aeb97de
achiewe <mamukashviliachi706@gmail.com>
2024-06-23 21:18:23 +0400

    add the burgerFrame component

--- src/app/components/BurgerFrame/BurgerFrame.tsx
+++ src/app/components/BurgerFrame/BurgerFrame.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import React from 'react'
 import { useSelector } from 'react-redux';
 
-// BurgerFrame
+// BurgerFrame component
 export default function BurgerFrame() {
     const openFrame = useSelector((store:RootState) => store.openFrame.openFrame)
   return (
