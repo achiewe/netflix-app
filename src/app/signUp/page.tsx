27aca39e5fabfeb8d6401caea747543d343a77e1
achiewe <mamukashviliachi706@gmail.com>
2024-06-23 21:21:56 +0400

    add signUp component

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import SignUpCard from '../components/SignUp/SignUpCard'
 
+
+// add signUp component
 export default function page() {
   return (
     <div className="flex justify-center items-center w-full h-screen bg-[url('../../public/assets/netflixBackground.png')] bg-no-repeat bg-cover relative">
