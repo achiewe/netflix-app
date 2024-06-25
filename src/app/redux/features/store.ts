94ee0f67b2fce1fa527f3bae9fb492fc460e96a6
achiewe <mamukashviliachi706@gmail.com>
2024-06-25 21:05:07 +0400

    add the store for every state to add

--- src/app/redux/features/store.ts
+++ src/app/redux/features/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import OpenBurgerSlice, { BurgerProps } from "./OpenBurgerSlice";
 
+// store for every states
 const store = configureStore({
     reducer:{
         openFrame:OpenBurgerSlice
