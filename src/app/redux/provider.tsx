30c1edcf75e7ecff4541084f4db92e6aa74a9eae
achiewe <mamukashviliachi706@gmail.com>
2024-06-26 21:32:50 +0400

    create file is provider and inside of it is children component to use states in the project

--- src/app/redux/provider.tsx
+++ src/app/redux/provider.tsx
@@ -2,6 +2,7 @@
 import { Provider } from "react-redux";
 import store from "./features/store";
 
+// add reduxProvider to use states inside the project and use there store file 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
   return <Provider store={store}>{children}</Provider>;
 }
