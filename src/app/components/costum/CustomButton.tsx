d0a0e23ae282b547703a1631b19fa3041d8b6a2e
achiewe <mamukashviliachi706@gmail.com>
2024-06-29 20:25:26 +0400

    add the customButton component to use it some child components

--- src/app/components/costum/CustomButton.tsx
+++ src/app/components/costum/CustomButton.tsx
@@ -5,6 +5,7 @@ buttonName:string;
 onClick: () => void;
 }
 
+// component customButton
 export default function CustomButton({buttonName, onClick}:CustomButtonProps) {
   return (
     <button className={`w-full text-white bg-[red] h-[40px] rounded-[5px] font-bold cursor-pointer hover:opacity-60`} onClick={onClick}> {buttonName} </button>
