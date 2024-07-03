979af731890bc20dc8743eccdf589e7c16ebbc99
achiewe <mamukashviliachi706@gmail.com>
2024-07-03 18:08:12 +0400

    add main home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 
+// main component
 export default function Home() {
   return <div className="w-full min-h-screen flex flex-col bg-[#000000]">
     <Header/>
