3e2c121c7f062de8e434b2272d5b80a2f6454e4e
achiewe <mamukashviliachi706@gmail.com>
2024-07-04 16:25:45 +0400

    add handleSignUpClick function

--- src/app/components/SignIn/SignInButton.tsx
+++ src/app/components/SignIn/SignInButton.tsx
@@ -18,6 +18,7 @@ export default function SignInButton() {
     window.location.href = '/';
   };
 
+  // handelSignUpClick function
   const handleSignUpClick = () => {
     console.log("Sign Up button clicked");
   
