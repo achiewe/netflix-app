e437bae08768187878cc7036dfc933f74f4f4033
achiewe <mamukashviliachi706@gmail.com>
2024-07-12 19:19:30 +0400

    create customButton function

--- src/app/components/costum/CustomButton.tsx
+++ src/app/components/costum/CustomButton.tsx
@@ -7,7 +7,7 @@ buttonName:string;
 onClick: () => void;
 }
 
-
+// CustomBUtton function
 export default function CustomButton({buttonName, onClick}:CustomButtonProps) {
   return (
     <button className={`w-full text-white bg-[red] h-[40px] rounded-[5px] font-bold cursor-pointer hover:opacity-60`} onClick={onClick}> {buttonName} </button>
